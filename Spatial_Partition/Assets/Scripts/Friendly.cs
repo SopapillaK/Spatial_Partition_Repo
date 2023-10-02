2b45578f2137227b18ed79a37397795bd8138509
SopapillaKat <vellita2011@gmail.com>
2023-10-02 11:37:34 -0700

    update timed not done

---
+++ Spatial_Partition/Assets/Scripts/Friendly.cs
@@ -0,0 +1,28 @@
+using SpatialPartitionPattern;
+using System.Collections;
+using UnityEngine;
+
+namespace SpatialPartitionPattern
+{
+    // the friendly sphere which is chasing the enemy cubes
+    public class Friendly : Soldier
+    {
+        //init friendly
+        public Friendly(GameObject soldierObj, float mapWidth)
+        {
+            this.soldierTrans = soldierObj.transform;
+
+            this.walkSpeed = 2f;
+        }
+
+
+        //Move towards the closest enemy - will always move within its grid
+        public override void Move(Soldier closestEnemy)
+        {
+            //Rotate towards the closest enemy
+            soldierTrans.rotation = Quaternion.LookRotation(closestEnemy.soldierTrans.position - soldierTrans.position);
+            //Move towards the closest enemy
+            soldierTrans.Translate(Vector3.forward * Time.deltaTime * walkSpeed);
+        }
+    }
+}
